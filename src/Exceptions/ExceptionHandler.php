14f2e32c33818fa277fc5f105863a986ca59fc67
davidbyoung <dbyoung2@gmail.com>
2019-08-13 19:40:07 -0500

    Rewrote exception handling to separate global exception handling from handling an exception thrown while processing a request (closes #9)

--- src/Exceptions/ExceptionHandler.php
+++ src/Exceptions/ExceptionHandler.php
@@ -12,202 +12,52 @@ declare(strict_types=1);
 
 namespace Aphiria\Api\Exceptions;
 
+use Aphiria\Middleware\IMiddleware;
+use Aphiria\Net\Http\Handlers\IRequestHandler;
 use Aphiria\Net\Http\IHttpRequestMessage;
-use Aphiria\Net\Http\IResponseWriter;
-use Aphiria\Net\Http\StreamResponseWriter;
-use ErrorException;
+use Aphiria\Net\Http\IHttpResponseMessage;
 use Exception;
-use function get_class;
-use function in_array;
-use Monolog\Handler\ErrorLogHandler;
-use Monolog\Logger;
-use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
 use Throwable;
 
 /**
- * Defines the exception handler
+ * Defines the middleware that handles exceptions
  */
-class ExceptionHandler implements IExceptionHandler
+final class ExceptionHandler implements IMiddleware
 {
-    /** @const The default name to use for the logger */
-    private const DEFAULT_LOGGER_NAME = 'app';
-    /** @var IExceptionResponseFactory The exception response factory */
-    protected IExceptionResponseFactory $exceptionResponseFactory;
-    /** @var LoggerInterface The logger */
-    protected LoggerInterface $logger;
-    /** @var ExceptionLogLevelFactoryRegistry The registry of exception log level factories */
-    protected ?ExceptionLogLevelFactoryRegistry $exceptionLogLevelFactories;
-    /** @var array The PSR-3 exception log level that will be logged */
-    protected ?array $exceptionLogLevels;
-    /** @var int The bitwise value of error levels that are to be logged */
-    protected int $errorLogLevels;
-    /** @var int The bitwise value of error levels that are to be thrown as exceptions */
-    protected int $errorThrownLevels;
-    /** @var IResponseWriter What to use to write a response */
-    protected IResponseWriter $responseWriter;
-    /** @var IHttpRequestMessage|null The current request, or null if there is none */
-    protected ?IHttpRequestMessage $request = null;
+    /** @var IExceptionResponseFactory The factory that create exception responses */
+    private IExceptionResponseFactory $exceptionResponseFactory;
+    /** @var IExceptionLogger The exception logger */
+    private IExceptionLogger $logger;
 
     /**
-     * @param IExceptionResponseFactory $exceptionResponseFactory The exception response factory
-     * @param LoggerInterface|null $logger The logger to use, or null if using the default error logger
-     * @param ExceptionLogLevelFactoryRegistry|null $exceptionLogLevelFactories The registry of exception log level factories
-     * @param array|null $exceptionLogLevels The PSR-3 exception log levels that will be logged, or null if
-     *      using the default levels
-     * @param int $errorLogLevels The bitwise value of error levels that are to be logged
-     * @param int $errorThrownLevels The bitwise value of error levels that are to be thrown as exceptions
-     * @param IResponseWriter $responseWriter What to use to write a response
+     * @param IExceptionResponseFactory $exceptionResponseFactory The factory that create exception responses
+     * @param IExceptionLogger|null $logger The exception logger
      */
-    public function __construct(
-        IExceptionResponseFactory $exceptionResponseFactory,
-        LoggerInterface $logger = null,
-        ExceptionLogLevelFactoryRegistry $exceptionLogLevelFactories = null,
-        array $exceptionLogLevels = null,
-        int $errorLogLevels = 0,
-        int $errorThrownLevels = E_ALL & ~(E_DEPRECATED | E_USER_DEPRECATED),
-        IResponseWriter $responseWriter = null
-    ) {
+    public function __construct(IExceptionResponseFactory $exceptionResponseFactory, IExceptionLogger $logger = null)
+    {
         $this->exceptionResponseFactory = $exceptionResponseFactory;
-        $this->logger = $logger ?? new Logger(self::DEFAULT_LOGGER_NAME, [new ErrorLogHandler()]);
-        $this->exceptionLogLevelFactories = $exceptionLogLevelFactories ?? new ExceptionLogLevelFactoryRegistry();
-        $this->exceptionLogLevels = $exceptionLogLevels ?? [
-            LogLevel::ERROR, LogLevel::CRITICAL, LogLevel::ALERT, LogLevel::EMERGENCY
-            ];
-        $this->errorLogLevels = $errorLogLevels;
-        $this->errorThrownLevels = $errorThrownLevels;
-        $this->responseWriter = $responseWriter ?? new StreamResponseWriter();
+        $this->logger = $logger ?? new ExceptionLogger();
     }
 
     /**
-     * @inheritdoc
+     * Handles a request
+     *
+     * @param IHttpRequestMessage $request The request to handle
+     * @param IRequestHandler $next The next request handler in the pipeline
+     * @return IHttpResponseMessage The response after the middleware was run
      */
-    public function handleError(
-        int $level,
-        string $message,
-        string $file = '',
-        int $line = 0,
-        array $context = []
-    ): void {
-        if ($this->shouldLogError($level)) {
-            $this->logger->log($level, $message, $context);
-        }
-
-        if ($this->shouldThrowError($level)) {
-            throw new ErrorException($message, 0, $level, $file, $line);
-        }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function handleException(Throwable $ex): void
+    public function handle(IHttpRequestMessage $request, IRequestHandler $next): IHttpResponseMessage
     {
-        // It's Throwable, but not an Exception
-        if (!$ex instanceof Exception) {
-            $ex = new FatalThrowableError($ex);
-        }
-
-        $logLevelFactory = $this->exceptionLogLevelFactories->getFactory(get_class($ex));
-        $logLevel = $logLevelFactory === null ? LogLevel::ERROR : $logLevelFactory($ex);
-
-        if ($this->shouldLogException($logLevel)) {
-            switch ($logLevel) {
-                case LogLevel::EMERGENCY:
-                    $this->logger->emergency($ex);
-                    break;
-                case LogLevel::ALERT:
-                    $this->logger->alert($ex);
-                    break;
-                case LogLevel::CRITICAL:
-                    $this->logger->critical($ex);
-                    break;
-                case LogLevel::ERROR:
-                    $this->logger->error($ex);
-                    break;
-                case LogLevel::WARNING:
-                    $this->logger->warning($ex);
-                    break;
-                case LogLevel::NOTICE:
-                    $this->logger->notice($ex);
-                    break;
-                case LogLevel::INFO:
-                    $this->logger->info($ex);
-                    break;
-                case LogLevel::DEBUG:
-                    $this->logger->debug($ex);
-                    break;
+        try {
+            return $next->handle($request);
+        } catch (Throwable $ex) {
+            if (!$ex instanceof Exception) {
+                $ex = new FatalThrowableError($ex);
             }
+
+            $this->logger->logException($ex);
+
+            return $this->exceptionResponseFactory->createResponseFromException($ex, $request);
         }
-
-        $response = $this->exceptionResponseFactory->createResponseFromException($ex, $this->request);
-        $this->responseWriter->writeResponse($response);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function handleShutdown(): void
-    {
-        $error = error_get_last();
-
-        if ($error !== null && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
-            $this->handleException(
-                new FatalErrorException($error['message'], $error['type'], 0, $error['file'], $error['line'])
-            );
-        }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function registerWithPhp(): void
-    {
-        ini_set('display_errors', 'off');
-        error_reporting(-1);
-        set_error_handler([$this, 'handleError']);
-        set_exception_handler([$this, 'handleException']);
-        register_shutdown_function([$this, 'handleShutdown']);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function setRequest(IHttpRequestMessage $request): void
-    {
-        $this->request = $request;
-    }
-
-    /**
-     * Determines whether or not the error level is loggable
-     *
-     * @param int $level The bitwise level
-     * @return bool True if the level is loggable, otherwise false
-     */
-    protected function shouldLogError(int $level): bool
-    {
-        return ($this->errorLogLevels & $level) !== 0;
-    }
-
-    /**
-     * Determines whether or not the exception level is loggable
-     *
-     * @param string $level The PSR-3 log level
-     * @return bool True if the level is loggable, otherwise false
-     */
-    protected function shouldLogException(string $level): bool
-    {
-        return in_array($level, $this->exceptionLogLevels, true);
-    }
-
-    /**
-     * Gets whether or not the error level is throwable
-     *
-     * @param int $level The bitwise level
-     * @return bool True if the level is throwable, otherwise false
-     */
-    protected function shouldThrowError(int $level): bool
-    {
-        return ($this->errorThrownLevels & $level) !== 0;
     }
 }
